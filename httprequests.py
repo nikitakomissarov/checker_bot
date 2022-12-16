a2d6d9db65d0885da53a3c3886309fea15c8eb77
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2022-12-16 22:30:09 +0300

    удалена лишняя функция, запрос зациклен в одной функции

--- httprequests.py
+++ httprequests.py
@@ -3,38 +3,20 @@ from requests.exceptions import ConnectTimeout
 from requests.exceptions import ReadTimeout
 from requests.exceptions import ConnectionError
 
-negative_message = ('Your task is not checked yet, continue....')
-positive_message = ('Your task is checked!')
-
-def timehack(devman_token, url, timestamp, **response_json):
-    while response_json['status'] != 'found':
-        print(negative_message)
+def request(devman_token, url, timestamp=None):
+    while True:
         try:
             headers = {"Authorization": devman_token, "timestamp_to_request": timestamp}
             response = requests.get(url, headers=headers)
             response.raise_for_status()
-            response_json = response.json()
-            print(response_json)
-            timestamp = str(response_json['timestamp_to_request'])
+            filtered_response = response.json()
+            if filtered_response['status'] != 'found':
+                timestamp = str(filtered_response['timestamp_to_request'])
+            else:
+                return filtered_response
         except ReadTimeout:
             pass
         except ConnectTimeout:
             pass
         except ConnectionError:
             pass
-    print("Your task is checked!")
-    return response_json
-
-def request(devman_token, url):
-    headers = {"Authorization": devman_token}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    response_json = response.json()
-    print(response_json)
-    if response_json['status'] != 'found':
-        timestamp = str(response_json['timestamp_to_request'])
-        response_json = timehack(devman_token, url, timestamp, **response_json)
-        return response_json
-    else:
-        print(positive_message)
-        return response_json
