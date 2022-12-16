1c35396cb5c618f469cdfab7e17b17f982e9f18d
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2022-12-16 22:29:50 +0300

    отправка сообщений передана в мейн

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telegram
 from httprequests import request
 from dotenv import dotenv_values
-from status import sending
 
 config = dotenv_values('.env')
 
@@ -9,14 +8,18 @@ DEVMAN_TOKEN = config['DEVMAN_TOKEN']
 TELEGRAM_TOKEN = config['TELEGRAM_TOKEN']
 URL = 'https://dvmn.org/api/long_polling/'
 
+
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     chat_id = bot.get_updates()[0]['message']['chat']['id']
-    print(f"The bot's starting, your chat id {chat_id}")
+    greet_message = f"The bot's starting, your chat id {chat_id}"
+    bot.send_message(text=greet_message, chat_id=chat_id)
 
     checkstatus = request(DEVMAN_TOKEN, URL)
-    sending(bot, chat_id, **checkstatus)
 
+    message = (
+        f'Преподаватель проверил работу {checkstatus["lesson_title"]}, она {"принята." if checkstatus["is_negative"] == "False" else "не принята, исправьте ошибки."} Ссылка на урок: {checkstatus["lesson_url"]}')
+    bot.send_message(text=message, chat_id=chat_id)
 
 if __name__ == "__main__":
     main()
