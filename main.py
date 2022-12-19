4f736792c5cbd8ccab87d36952745f2f7365f4b3
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2022-12-19 20:33:07 +0300

    изменил получение chat_id, новое имя словаря, разбил сообщение

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from dotenv import dotenv_values
 from requests.exceptions import ConnectTimeout
 from requests.exceptions import ReadTimeout
 from requests.exceptions import ConnectionError
+import time
 
 config = dotenv_values('.env')
 
 DEVMAN_TOKEN = config['DEVMAN_TOKEN']
 TELEGRAM_TOKEN = config['TELEGRAM_TOKEN']
+CHAT_ID = config['CHAT_ID']
 URL = 'https://dvmn.org/api/long_polling/'
-
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    chat_id = bot.get_updates()[0]['message']['chat']['id']
-    greet_message = f"The bot's starting, your chat id {chat_id}"
-    bot.send_message(text=greet_message, chat_id=chat_id)
+    greet_message = f"The bot's starting, your chat id {CHAT_ID}"
+    bot.send_message(text=greet_message, chat_id=CHAT_ID)
     timestamp = None
 
     while True:
@@ -24,18 +24,22 @@ def main():
             headers = {"Authorization": DEVMAN_TOKEN, "timestamp_to_request": timestamp}
             response = requests.get(URL, headers=headers)
             response.raise_for_status()
-            filtered_response = response.json()
-            if filtered_response['status'] != 'found':
-                timestamp = str(filtered_response['timestamp_to_request'])
+            response_dict = response.json()
+            if response_dict['status'] != 'found':
+                timestamp = str(response_dict['timestamp_to_request'])
             else:
                 message = (
-                    f'Преподаватель проверил работу {filtered_response["lesson_title"]}, она {"принята." if filtered_response["is_negative"] == "False" else "не принята, исправьте ошибки."} Ссылка на урок: {filtered_response["lesson_url"]}')
-                bot.send_message(text=message, chat_id=chat_id)
+                    f'Преподаватель проверил работу {response_dict["lesson_title"]},'
+                    f'она {"принята." if response_dict["is_negative"] == "False" else "не принята, исправьте ошибки."}'
+                    f'Ссылка на урок: {response_dict["lesson_url"]}'
+                )
+                bot.send_message(text=message, chat_id=CHAT_ID)
         except ReadTimeout:
             pass
         except ConnectTimeout:
             pass
         except ConnectionError:
+            time.sleep(5)
             pass
 
 if __name__ == "__main__":
